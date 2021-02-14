a07477656a8e7acbc1032ca7ac2af0e4d0c340b5
Stephane Seng <seng.stephane@gmail.com>
2021-02-14 15:25:54 +0100

    Deprecated implementation.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Payload;
 
 @SpringBootApplication
+@EnableBinding(DemoApplication.Source.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	interface Source {
+		@Input("myChannel")
+		MessageChannel input();
+	}
+
+	@StreamListener("myChannel")
+	protected void consumer(@Payload String message) {
+		System.out.println(message);
+	}
 }
